956fb0f1b2f1cff665ee2c70a03f6244820e4c31
OmkarKadoli <omkarkadoli@gmail.com>
2023-08-01 14:41:56 +0530

    Upload Addtion of array Example file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port =process.env.PORT || 3000;
-const path = require('path');
+// const path = require('path');
+const add = require("./sum.js");  
 
 app.use(express.static('html/'));
+
 
 app.listen(port, () =>{
     console.log(`Server Running on port ${port}`);
---
+++ sum.js
@@ -0,0 +1,10 @@
+let arr = [4, 8, 7, 13, 12]
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+
+console.log("Addition of array is : " + sum);
+
+module.exports = {sum};
